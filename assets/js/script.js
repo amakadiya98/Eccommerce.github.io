b8d7942fe1256760d53160e4dd6b2d3efc3d3d16
Sagar Ratanpara <sagar@closedigit.com>
2023-07-15 15:30:20 +0530

    header fixed top done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,26 @@ var isSubSubMenuActive = false;
 var isRegoActive = false;
 var isVehicleActive = false;
 
+$(document).on("click", function (event) {
+  var target = $(event.target);
+  if (
+    !target.closest(".menu-button").length &&
+    !target.closest(".menu-modal").length &&
+    !target.closest(".menu-modal-two").length &&
+    !target.closest(".menu-modal-three").length
+  ) {
+    isMainMenuActive = false;
+    isSubMenuActive = false;
+    isSubSubMenuActive = false;
+    $(".menu-button").removeClass("hide");
+    $(".close-button").addClass("hide");
+    $(".menu-modal").addClass("hide");
+    $(".menu-modal-two").slideUp(300).removeClass("two-active");
+    $(".menu-modal-three").slideUp(300).removeClass("three-active");
+    $("body").removeClass("overflow-hidden");
+  }
+});
+
 $(".menu-button").click(function () {
   isMainMenuActive = true;
   isSubMenuActive = false;
@@ -26,18 +46,24 @@ $(".close-button").click(function () {
   $("body").removeClass("overflow-hidden");
 });
 
-$(".menu-modal-two").parent("li").click(function () {
-  isSubMenuActive = true;
-  $(this).find(">.menu-modal-two").not(':animated').slideDown(300);
-  $(this).find(">.menu-modal-two").addClass("two-active");
-});
+$(".menu-modal-two")
+  .parent("li")
+  .click(function () {
+    isSubMenuActive = true;
+    $(this).find(">.menu-modal-two").not(":animated").slideDown(300);
+    $(this).find(">.menu-modal-two").addClass("two-active");
+  });
 
-$(".menu-modal-three").parent("li").click(function () {
-  isSubSubMenuActive = true;
-  $(".menu-modal-three.three-active").slideUp(300).removeClass("three-active");
-  $(this).find(">.menu-modal-three").not(':animated').slideDown(300);
-  $(this).find(">.menu-modal-three").addClass("three-active");
-});
+$(".menu-modal-three")
+  .parent("li")
+  .click(function () {
+    isSubSubMenuActive = true;
+    $(".menu-modal-three.three-active")
+      .slideUp(300)
+      .removeClass("three-active");
+    $(this).find(">.menu-modal-three").not(":animated").slideDown(300);
+    $(this).find(">.menu-modal-three").addClass("three-active");
+  });
 
 $(".menu-modal-two > .common-title > button").click(function () {
   isSubMenuActive = false;
@@ -58,10 +84,11 @@ $(".main-menu-list").click(function () {
       $(".menu-modal-two").slideUp(300).removeClass("two-active");
     }
   }
-})
+});
 
 $("#rego").click(function () {
   $(".mobile-vehicle.active").removeClass("active");
+  $("body").addClass("overflow-hidden");
   isVehicleActive = false;
   if (isRegoActive) {
     $(".form-block.rego").slideUp(300).removeClass("active-form");
@@ -72,11 +99,12 @@ $("#rego").click(function () {
     $(".form-block.rego").slideDown(300).addClass("active-form");
     isRegoActive = true;
   }
-})
+});
 
 $("#vehicle").click(function () {
   if (isVehicleActive) {
     $(".form-block.vehicle").slideUp(300).removeClass("active-form");
+    $("body").addClass("overflow-hidden");
     isVehicleActive = false;
   } else {
     $(".mobile-vehicle").addClass("active");
@@ -86,16 +114,18 @@ $("#vehicle").click(function () {
     $(".form-block.vehicle").slideDown(300).addClass("active-form");
     isVehicleActive = true;
   }
-})
-
+});
 
 $("#searchInput").focus(function () {
-  $(".search-result").slideDown(300).addClass('show');
-})
+  $(".search-result").slideDown(300).addClass("show");
+});
 
 $(document).on("click", function (event) {
-  if (!$(event.target).closest(".content-wrapper").length && !$(event.target).closest("#searchField").length) {
-    $(".search-result").slideUp(300).removeClass('show');
+  if (
+    !$(event.target).closest(".content-wrapper").length &&
+    !$(event.target).closest("#searchField").length
+  ) {
+    $(".search-result").slideUp(300).removeClass("show");
   }
 });
 
@@ -111,27 +141,26 @@ $("#vehicleBtn").click(function (event) {
   event.preventDefault();
   $(".my-garage > .rego-modal").not(":animated").addClass("hide");
   $(".my-garage > .vehicle-modal.hide").not(":animated").removeClass("hide");
-})
+});
 $("#backToRego").click(function (event) {
   event.preventDefault();
   $(".my-garage > .vehicle-modal").not(":animated").addClass("hide");
   $(".my-garage >.rego-modal.hide").not(":animated").removeClass("hide");
-})
+});
 
 $(".my-garage").click(function (event) {
-  if(!$(event.target).closest(".modal-block").length) {
+  if (!$(event.target).closest(".modal-block").length) {
     $(".my-garage").slideUp(300).removeClass("show");
     $("body").removeClass("overflow-hidden");
   }
-})
+});
 
 $("#cartBtn").click(function () {
   $("body").addClass("overflow-hidden");
   $(".cart-section").slideDown(300).addClass("show");
-})
+});
 
-$("#cart-close").click(function() {
+$("#cart-close").click(function () {
   $(".cart-section").slideUp(300).removeClass("show");
   $("body").removeClass("overflow-hidden");
-})
-
+});
